1980c53d5bf1eeb596be2d284943fd4669d1dc4b
Richard Walsh <richwalsh2@googlemail.com>
2021-07-18 13:00:59 +0100

    initial home page responsiveness

--- src/home.js
+++ src/home.js
@@ -1,33 +1,22 @@
-import {removeChild, buttonActive} from './utilities'
-
-
+import { removeChild, buttonActive } from './utilities';
 
 const homeContent = () => {
-    const contentGrid = document.querySelector('.content__grid');
-    const htmlHeading = `<h2 class="heading__secondary home__heading" id="hover">WELCOME!</h2>`;
-    const htmlImage = `<img src="img/hillary_welcome.jpg" alt="Picture of the owner" class="home__img tab-fade">`
-    const htmlText = `<div class="home__text tab-fade">It's like I always say. I'm a business man</div> --></img>`
-    const content = document.querySelector('.content')
+  const contentGrid = document.querySelector('.content__grid');
+  const htmlHeading = `<h2 class="home__heading heading__secondary" id="hover">WELCOME!</h2>`;
+  const htmlImage = `<img src="img/hillary_welcome.jpg" alt="Picture of the owner" class="home__img tab-fade"></img>`;
+  const htmlText = `<div class="home__text tab-fade">It's like I always say. I'm a business man</div>`;
+  const content = document.querySelector('.content');
 
+  removeChild(content);
 
-    
+  buttonActive();
 
-    removeChild(content)
-    
-    buttonActive()
+  contentGrid.style.display = 'flex';
+  contentGrid.style.flexDirection = 'column';
 
-    contentGrid.style.display="flex"
-    contentGrid.style.flexDirection="column"
-
-    content.insertAdjacentHTML('beforeend', htmlHeading);
-    content.insertAdjacentHTML('beforeend', htmlImage);
-    content.insertAdjacentHTML('beforeend', htmlText);
-    
-    
-    
-    
-}
-
-
+  content.insertAdjacentHTML('beforeend', htmlHeading);
+  content.insertAdjacentHTML('beforeend', htmlImage);
+  content.insertAdjacentHTML('beforeend', htmlText);
+};
 
 export default homeContent;
