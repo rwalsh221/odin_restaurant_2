d82b0cb2fa4d2307ba678ad4d71379dba0ff4304
Richard Walsh <richwalsh2@googlemail.com>
2021-07-18 16:17:55 +0100

    initial about  responsiveness

--- src/about.js
+++ src/about.js
@@ -1,26 +1,23 @@
-import {removeChild, buttonActive} from './utilities'
-
+import { removeChild, buttonActive } from './utilities';
 
 const aboutContent = () => {
-    const htmlImage = `<div class="about__img tab-fade"><img src="img/briss_1.jpg" alt="picture of owner" class="content__img img__top"><img src="img/shop.jpg" alt="picture of shop" class="content__img img__bottom"></div>`
-    const htmlText = `<div class="content__text tab-fade"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque esse tenetur, architecto nulla in magnam dignissimos optio hic eveniet nesciunt ipsum ullam inventore ab laboriosam aspernatur! Beatae deleniti vel adipisci aperiam ex at? Possimus mollitia recusandae sequi magni expedita quia nemo culpa cum placeat nobis sint, eveniet ullam optio cupiditate ut. Eligendi doloremque rerum facilis quisquam a dolor similique veniam atque quod officia consectetur dignissimos ipsum veritatis, libero et maxime molestiae numquam vitae nemo rem? Esse ab possimus inventore! Eius velit, soluta assumenda non dicta labore fugit laudantium tempora aspernatur, officia asperiores blanditiis explicabo suscipit maiores quas nesciunt praesentium.</p></div>`
-    const contentGrid = document.querySelector('.content__grid');
-    const content = document.querySelector('.content');
-    
+  const htmlImage = `<div class="about__img--container tab-fade"><img src="img/briss_1.jpg" alt="picture of owner" class="about__img img__top"><img src="img/shop.jpg" alt="picture of shop" class="about__img img__bottom"></div>`;
+  const htmlText = `<div class="about__text tab-fade"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque esse tenetur, architecto nulla in magnam dignissimos optio hic eveniet nesciunt ipsum ullam inventore ab laboriosam aspernatur! Beatae deleniti vel adipisci aperiam ex at? Possimus mollitia recusandae sequi magni expedita quia nemo culpa cum placeat nobis sint, eveniet ullam optio cupiditate ut. Eligendi doloremque rerum facilis quisquam a dolor similique veniam atque quod officia consectetur dignissimos ipsum veritatis, libero et maxime molestiae numquam vitae nemo rem? Esse ab possimus inventore! Eius velit, soluta assumenda non dicta labore fugit laudantium tempora aspernatur, officia asperiores blanditiis explicabo suscipit maiores quas nesciunt praesentium.</p></div>`;
+  const contentGrid = document.querySelector('.content__grid');
+  const content = document.querySelector('.content');
 
-    removeChild(content);
-    buttonActive('btn__about')
+  removeChild(content);
+  buttonActive('btn__about');
 
-    contentGrid.style.display="grid"
-    contentGrid.style.gridTemplateColumns="35% 65%"
-    contentGrid.style.gridTemplateRows="max-content 100%"
-    contentGrid.style.gridTemplateAreas='"nav-bar nav-bar" "img content-text"'
-    
-    content.insertAdjacentHTML('beforeend', htmlImage)
-    content.insertAdjacentHTML('beforeend', htmlText)
+  contentGrid.style.display = 'grid';
+  contentGrid.style.gridTemplateColumns = '35% 65%';
+  contentGrid.style.gridTemplateRows = 'max-content 100%';
+  contentGrid.style.gridTemplateAreas = '"nav-bar nav-bar" "img content-text"';
 
-    console.log('ABBOOUUTT')
+  content.insertAdjacentHTML('beforeend', htmlImage);
+  content.insertAdjacentHTML('beforeend', htmlText);
+
+  console.log('ABBOOUUTT');
 };
 
 export default aboutContent;
-
