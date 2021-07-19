18825edc5fb40d431c398a38cd629edd533bfe89
Richard Walsh <richwalsh2@googlemail.com>
2021-07-19 14:03:32 +0100

    change html structure for sidebar and main grid

--- src/menu.js
+++ src/menu.js
@@ -1,59 +1,115 @@
-import {removeChild, buttonActive} from './utilities'
+import { removeChild, buttonActive } from './utilities';
 
 //Sets initial html for menu page
 const menuSkeleton = () => {
-    const contentGrid = document.querySelector('.content__grid');
+  const contentGrid = document.querySelector('.content__grid');
 
-    const content = document.querySelector('.content')
+  const content = document.querySelector('.content');
 
-    const menuImageHtml = `<div class="menu__img tab-fade"></div>`
+  const menuImageHtml = `<div class="menu__img tab-fade"></div>`;
 
-    const menuContentHtml = `<div class="menu__content tab-fade"><div class="menu__heading"><h3 class="heading__tertiary menu__heading--main">MENU</h3><h4 class="menu__heading--secondary">All our products are locally sourced</h4></div><div class="menu__list"><ul class="menu__list--item" id="menu-list"></ul><ul class="menu__list--price"></ul></div></div>`
+  const menuContentHtml = `<div class="menu__content tab-fade"><div class="menu__heading"><h3 class="heading__tertiary menu__heading--main">MENU</h3><h4 class="menu__heading--secondary">All our products are locally sourced</h4></div><div class="menu__list"><ul class="menu__list--item" id="menu-list"></ul><ul class="menu__list--price"></ul></div></div>`;
 
-    removeChild(content)
+  removeChild(content);
 
-    buttonActive('btn__menu')
+  buttonActive('btn__menu');
 
-    contentGrid.removeAttribute('style');
+  contentGrid.removeAttribute('style');
 
-    contentGrid.style.display="flex"
+  contentGrid.style.display = 'flex';
 
-    content.insertAdjacentHTML("beforeend", menuImageHtml);
-    content.insertAdjacentHTML("beforeend", menuContentHtml);
+  const screenWidth = window.matchMedia('(max-width: 414px)');
+  if (screenWidth.matches) {
+    console.log('MATCH');
+    contentGrid.style.flexDirection = 'column';
+  }
 
-}
+  content.insertAdjacentHTML('beforeend', menuImageHtml);
+  content.insertAdjacentHTML('beforeend', menuContentHtml);
+};
 
 // (id to match image for animation, name of product, price of product, kilogram true or false, image src)
 const menuFactory = (id, name, price, kiloGram, imageSrc) => {
-    const imageHtml = `<div class="menu__img--${id} menu__img--ovflow"><img src="${imageSrc}" alt="Image of ${name}" class="img__size" id="menu__img--${id}"></div>`
+  const imageHtml = `<div class="menu__img--${id} menu__img--ovflow"><img src="${imageSrc}" alt="Image of ${name}" class="img__size" id="menu__img--${id}"></div>`;
 
-    const menuItemHtml =`<li class="menu__item" id="hover-${id}">${name}</li>`
+  const menuItemHtml = `<li class="menu__item" id="hover-${id}">${name}</li>`;
 
-    let menuPriceHtml =``
+  let menuPriceHtml = ``;
 
-    if (kiloGram === true) {
-        menuPriceHtml = `<li class="menu__price">£${price} / KG</li>`
-    }   else {
-        menuPriceHtml = `<li class="menu__price">£${price}</li>`
-    }
+  if (kiloGram === true) {
+    menuPriceHtml = `<li class="menu__price">£${price} / KG</li>`;
+  } else {
+    menuPriceHtml = `<li class="menu__price">£${price}</li>`;
+  }
 
-    const render = () => {
-    document.querySelector('.menu__img').insertAdjacentHTML("beforeend", imageHtml);
-    document.querySelector('.menu__list--item').insertAdjacentHTML("beforeend", menuItemHtml);
-    document.querySelector('.menu__list--price').insertAdjacentHTML("beforeend", menuPriceHtml)
-    }
+  const render = () => {
+    document
+      .querySelector('.menu__img')
+      .insertAdjacentHTML('beforeend', imageHtml);
+    document
+      .querySelector('.menu__list--item')
+      .insertAdjacentHTML('beforeend', menuItemHtml);
+    document
+      .querySelector('.menu__list--price')
+      .insertAdjacentHTML('beforeend', menuPriceHtml);
+  };
 
-    return { render } 
+  return { render };
+};
 
-}
-
-const sausage = menuFactory(1, 'Directors Sausage\'s', '3.40 / 6', false, './img/dir_sausage.jpg');
-const pPudding = menuFactory(2, 'Pease Pudding', '3.50', false, './img/pease-pudding-photo.jpeg');
-const rSteak = menuFactory(3, 'Rump Steak', '14.99', true, './img/rump_steak.jpg');
+const sausage = menuFactory(
+  1,
+  "Directors Sausage's",
+  '3.40 / 6',
+  false,
+  './img/dir_sausage.jpg'
+);
+const pPudding = menuFactory(
+  2,
+  'Pease Pudding',
+  '3.50',
+  false,
+  './img/pease-pudding-photo.jpeg'
+);
+const rSteak = menuFactory(
+  3,
+  'Rump Steak',
+  '14.99',
+  true,
+  './img/rump_steak.jpg'
+);
 const sRib = menuFactory(4, 'Spare Ribs', '6.33', true, './img/spare_ribs.jpg');
-const bPork = menuFactory(5, 'Blade Pork', '5.99', true, './img/blade_pork.jpg');
-const legLamb = menuFactory(6, 'Leg of Lamb', '8.99', true, './img/leg_lamb.jpg');
+const bPork = menuFactory(
+  5,
+  'Blade Pork',
+  '5.99',
+  true,
+  './img/blade_pork.jpg'
+);
+const legLamb = menuFactory(
+  6,
+  'Leg of Lamb',
+  '8.99',
+  true,
+  './img/leg_lamb.jpg'
+);
 const chicken = menuFactory(7, 'Chicken', '4.17', true, './img/chicken.jpg');
-const special = menuFactory(8, 'Special Stuff', '32.40', true, './img/special.jpg')
+const special = menuFactory(
+  8,
+  'Special Stuff',
+  '32.40',
+  true,
+  './img/special.jpg'
+);
 
-export {menuSkeleton, sausage, pPudding, rSteak, sRib, bPork, legLamb, chicken, special}
+export {
+  menuSkeleton,
+  sausage,
+  pPudding,
+  rSteak,
+  sRib,
+  bPork,
+  legLamb,
+  chicken,
+  special,
+};
