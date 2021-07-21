52d3365e24a5435b0a180478e3b9e22f70315c28
Richard Walsh <richwalsh2@googlemail.com>
2021-07-21 11:31:05 +0100

    fix contact image responsiveness

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,9 @@ import { removeChild, buttonActive } from './utilities';
 import displayMap from './map.js';
 
 const contactContent = () => {
-  const htmlMap = `<section class="map-container tab-fade"><div class="map" id='map'></div></section>`;
+  const htmlMap = `<div class="map-container tab-fade"><div class="map" id='map'></div></div>`;
   const htmlContact = `<div class="contact tab-fade"><h3 class="contact__heading heading__tertiary">COME AND HAVE A BUTCHERS!</h3><ul class="contact__address"><li>H. BRISS & SON</li><li>86 Station RD</li><li>Royston Vasey</li><li>England</li><li>01457 555555</li></ul></div>`;
-  const htmlImage = `<img src="./img/Royston.jpg" alt="Welcome to royston vasey" class="contact__img tab-fade">`;
+  const htmlImage = `<div class="contact__img--container tab-fade"><img src="./img/Royston.jpg" alt="Welcome to royston vasey" class="contact__img"></div>`;
   const contentGrid = document.querySelector('.content__grid');
   const content = document.querySelector('.content');
 
